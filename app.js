f5a985b27daebeccbe42b8ac3ebd4f5fd0d538ab
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2022-12-12 14:08:31 +0000

    MVP version complete

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "blue", "rgba(133,122,200", "f15025"];
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
 const btn = document.getElementById('btn')
 const color = document.querySelector(".color")
